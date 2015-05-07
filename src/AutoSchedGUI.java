7a00642a149aca1d4ab05e9d721d9eb44274de73
Douglas Manzelmann <dougamanzelmann@gmail.com>
2015-05-07 16:48:27 -0400

    More GUI Work. Back to refactoring TMS Sched. OOP stuff.

--- src/AutoSchedGUI.java
+++ src/AutoSchedGUI.java
@@ -3,25 +3,33 @@ import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.util.List;
 
 /**
  * Created by dmanzelmann on 5/6/2015.
  */
 public class AutoSchedGUI extends JFrame  {
     AutoSched sched;
+    List<Listing> schedule;
 
     public AutoSchedGUI() throws InterruptedException {
         System.setProperty("jsse.enableSNIExtension", "false");
         sched = new AutoSched();
+        JPanel displaySchedulePanel = new JPanel(new GridLayout(0, 1));
+        JScrollPane listingScrollPane = new JScrollPane(displaySchedulePanel,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        //JPanel schedulePanel = new JPanel();
+        //JTextArea listingTextArea = new JTextArea();
 
-        pack();
+
+        setLayout(new BorderLayout());
         setLocationRelativeTo(null);
         setTitle("Weekly Schedule");
         setSize(800, 800);
         setMinimumSize(new Dimension(800, 800));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setLayout(new BorderLayout());
 
         // Panel Read Sched
         JPanel readSchedPanel = new JPanel();
@@ -84,8 +92,33 @@ public class AutoSchedGUI extends JFrame  {
                 // will need to refactor code for a char[] later.
                 String password = new String(passwordField.getPassword());
 
-                sched.visitPortalWeek(year, month, day);
-                sched.loginToPortal(userNameField.getText(), password);
+                //sched.visitPortalWeek(year, month, day);
+                //sched.loginToPortal(userNameField.getText(), password);
+                sched.readSched(userNameField.getText(), password, year, month, day);
+                schedule = sched.getListings();
+
+
+                for (Listing l : schedule) {
+                    JButton current = new JButton();
+                    current.setHorizontalAlignment(SwingConstants.LEFT);
+
+                    if (l.getActivity().equals("Mediasite"))
+                        current.setBackground(Color.GREEN);
+                    else if (l.getActivity().equals("Videoconference"))
+                        current.setBackground(Color.CYAN);
+                    else if (l.getActivity().equals("Pre-record"))
+                        current.setBackground(Color.ORANGE);
+                    else
+                        current.setBackground(Color.gray);
+
+                    current.setText(l.toString());
+                    displaySchedulePanel.add(current);
+
+                }
+
+                listingScrollPane.repaint();
+                revalidate();
+                repaint();
             }
         });
 
@@ -99,6 +132,7 @@ public class AutoSchedGUI extends JFrame  {
                         BorderFactory.createEtchedBorder(
                                 EtchedBorder.RAISED, Color.GRAY, Color.DARK_GRAY),
                         "TMS Login"));
+
         JLabel tmsUserNameLabel = new JLabel("Enter username");
         JTextField tmsUserNameField = new JTextField(10);
         JLabel tmsPasswordLabel = new JLabel("Enter password");
@@ -107,10 +141,39 @@ public class AutoSchedGUI extends JFrame  {
         tmsSchedPanel.add(tmsUserNameField);
         tmsSchedPanel.add(tmsPasswordLabel);
         tmsSchedPanel.add(tmsPasswordField);
-        tmsSchedPanel.add(readSchedPanelLogin);
 
-        add(readSchedPanel);
-        add(tmsSchedPanel);
+        // Create a login panel and add the two login sections
+        JPanel loginPanel = new JPanel(new BorderLayout());
+        loginPanel.add(readSchedPanel, BorderLayout.PAGE_START);
+        loginPanel.add(tmsSchedPanel, BorderLayout.PAGE_END);
+
+        // Create a content panel and add the individual panels
+
+
+        // Display the content of the schedule
+        //JPanel displaySchedulePanel = new JPanel(new GridLayout(0, 1));
+        //JTextArea listingTextArea = new JTextArea();
+        //listingTextArea.setText("");
+        //JScrollPane listingScrollPane = new JScrollPane(displaySchedulePanel);
+        //JPanel schedulePanel = new JPanel();
+        //schedulePanel.add(listingScrollPane);
+       //displaySchedulePanel.add(listingScrollPane);
+
+        // Display Progress Bars
+        JProgressBar readSchedProgressBar = new JProgressBar();
+        readSchedProgressBar.setStringPainted(true);
+        JProgressBar mediasiteSchedProgressBar = new JProgressBar();
+        mediasiteSchedProgressBar.setStringPainted(true);
+        JProgressBar tmsSchedProgressBar = new JProgressBar();
+        tmsSchedProgressBar.setStringPainted(true);
+
+
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        contentPanel.add(loginPanel, BorderLayout.PAGE_START);
+        contentPanel.add(listingScrollPane, BorderLayout.CENTER);
+        add(contentPanel);
+        pack();
+        setVisible(true);
         validate();
     }
 
